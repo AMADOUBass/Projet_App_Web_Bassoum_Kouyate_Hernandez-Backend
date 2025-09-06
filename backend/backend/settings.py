6a28c32b9289dd02b9fcd32ef1345ac44e98f090
Amadou Bassoum <bassoumamadou0@gmail.com>
2025-09-06 13:39:32 -0400

    Ajout du CORS pour faire communiquer les deux projets

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -70,7 +72,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:3000",
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATIC_URL = '/static/'
