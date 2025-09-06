e02bb2ef892120dfb6b5f5be7bb283e8f56e0b2f
Amadou Bassoum <bassoumamadou0@gmail.com>
2025-09-06 12:58:11 -0400

    Automatiser la création d'un admin dans build.sh pour le déploiement

--- backend/build.sh
+++ backend/build.sh
@@ -6,6 +6,16 @@ python manage.py collectstatic --noinput
 
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --noinput
+# Create superuser if requested
+if [[ $CREATE_SUPERUSER == "true" ]]; then
+    python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser(
+        '$DJANGO_SUPERUSER_USERNAME',
+        '$DJANGO_SUPERUSER_EMAIL',
+        '$DJANGO_SUPERUSER_PASSWORD'
+    )
+"
 fi
