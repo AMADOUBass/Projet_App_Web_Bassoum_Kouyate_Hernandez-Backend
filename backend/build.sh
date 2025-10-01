c1762b6916b736f20f10e08222c184c04c1dacad
Amadou Bassoum <bassoumamadou0@gmail.com>
2025-10-01 00:36:34 -0400

    Refactor build.sh for superuser creation
    Updated build script to enable superuser creation.

--- backend/build.sh
+++ backend/build.sh
@@ -1,24 +1,12 @@
-# set -o errexit
+set -o errexit
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
-# python manage.py migrate
+python manage.py migrate
 
-# # Create superuser if requested
-# if [[ $CREATE_SUPERUSER == "true" ]]; then
-#     python manage.py shell -c "
-# from django.contrib.auth import get_user_model
-# User = get_user_model()
-# if not User.objects.filter(email='$DJANGO_SUPERUSER_EMAIL').exists():
-#     User.objects.create_superuser(
-#         email='$DJANGO_SUPERUSER_EMAIL',
-#         password='$DJANGO_SUPERUSER_PASSWORD'
-#     )
-# "
-# fi
-# echo "Superuser created with email: $DJANGO_SUPERUSER_EMAIL"
+# Create superuser if requested
 if [ "$CREATE_SUPERUSER" = "true" ]; then
   python manage.py createsuperuser \
     --noinput \
@@ -26,3 +14,5 @@ if [ "$CREATE_SUPERUSER" = "true" ]; then
     --username "$DJANGO_SUPERUSER_EMAIL" \
     || echo "Superuser already exists, skipping."
 fi
+echo "Superuser created with email: $DJANGO_SUPERUSER_EMAIL"
+
